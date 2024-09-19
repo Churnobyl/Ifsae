70e01f7cbe88c2594591c6a695975ca49686cb1a
성철민 <tjdcjfals3@naver.com>
2024-09-19 13:33:12 +0900

    feat: s3 add

--- ifsavedog-BE/src/main/java/com/easteregg/ifsae/global/config/S3Config.java
+++ ifsavedog-BE/src/main/java/com/easteregg/ifsae/global/config/S3Config.java
@@ -4,6 +4,7 @@ import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -22,13 +23,12 @@ public class S3Config {
     private String secretKey;
 
     @Bean
-    public AmazonS3 amazonS3() {
-        AWSCredentials awsCredentials = new BasicAWSCredentials(accessKey, secretKey);
-
-        return AmazonS3ClientBuilder.standard()
-                                    .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
-                                    .withRegion(region)
-                                    .build();
+    public AmazonS3Client amazonS3() {
+        BasicAWSCredentials awsCredentials= new BasicAWSCredentials(accessKey, secretKey);
+        return (AmazonS3Client)AmazonS3ClientBuilder.standard()
+                .withRegion(region)
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
     }
 
 }
