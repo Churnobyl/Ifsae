cba66cb9fb35be2a4d8cf588bdcf40f6ced76cb3
kgc91747 <kgc91747@naver.com>
2024-09-23 13:47:56 +0900

    [BE]Feat: Dog Controller 일부 구현
    DogController
    - 강아지 등록, 수정, 조회 기능 구현
    
    [TODO]
    - 강아지 정보 삭제 기능 추가
    - 강아지 상태 변경 기능 추가
    ex) 입양, 임시보호 종료 등

--- ifsavedog-BE/src/main/java/com/easteregg/ifsae/domain/dog/controller/DogController.java
+++ ifsavedog-BE/src/main/java/com/easteregg/ifsae/domain/dog/controller/DogController.java
@@ -1,11 +1,23 @@
 package com.easteregg.ifsae.domain.dog.controller;
 
+import com.easteregg.ifsae.domain.dog.dto.DogCreateRequest;
+import com.easteregg.ifsae.domain.dog.entity.Dog;
 import com.easteregg.ifsae.domain.dog.service.DogService;
-import com.easteregg.ifsae.global.s3.S3ImageUploader;
+import com.easteregg.ifsae.domain.user.entity.User;
+import com.easteregg.ifsae.global.dto.CommonSuccessResponse;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/dog")
@@ -15,6 +27,30 @@ public class DogController {
 
     private final DogService dogService;
 
-    private final S3ImageUploader s3ImageUploader;
+    @PostMapping
+    public ResponseEntity<CommonSuccessResponse> createDog(@RequestPart DogCreateRequest dogCreateRequest,
+                                                           @RequestPart MultipartFile file) throws IOException {
+
+        Dog dog = dogService.createDog(new User(), dogCreateRequest);
+        dogService.updateDogProfileImage(file, dog);
+
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping("{dogId}")
+    public ResponseEntity<?> getDogDetail(@PathVariable long dogId) {
+        return new ResponseEntity<>(dogService.findById(dogId), HttpStatus.OK);
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getDogListByName(@RequestParam String name) {
+        return new ResponseEntity<>(dogService.findDogsByName(name), HttpStatus.OK);
+    }
+
+    @GetMapping("/shelter/{shelterId}")
+    public ResponseEntity<?> getDogListByShelterId(@PathVariable long shelterId) {
+        return new ResponseEntity<>(dogService.findDogsByShelterId(shelterId), HttpStatus.OK);
+    }
+
 
 }
