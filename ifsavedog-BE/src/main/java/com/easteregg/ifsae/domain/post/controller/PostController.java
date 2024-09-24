99231533a5d93179afb1d8b26572ebcabde7a8b7
성철민 <tjdcjfals3@naver.com>
2024-09-24 14:38:06 +0900

    [BE][Feat] PostController Update

--- ifsavedog-BE/src/main/java/com/easteregg/ifsae/domain/post/controller/PostController.java
+++ ifsavedog-BE/src/main/java/com/easteregg/ifsae/domain/post/controller/PostController.java
@@ -1,11 +1,19 @@
 package com.easteregg.ifsae.domain.post.controller;
 
+import com.easteregg.ifsae.domain.post.dto.PostDto;
 import com.easteregg.ifsae.domain.post.service.PostService;
+import com.easteregg.ifsae.domain.user.entity.User;
+import com.easteregg.ifsae.global.dto.CommonSuccessResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping("/api/post")
@@ -15,4 +23,40 @@ public class PostController {
 
     private final PostService postService;
 
+    @GetMapping("/{postId}")
+    public ResponseEntity<?> getPost(@PathVariable("postId") Long postId) {
+        PostDto.Response response = postService.read(postId);
+
+        if (response == null) {
+            return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+
+    @PreAuthorize("hasAuthority('user')")
+    @PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
+    public ResponseEntity<?> writePost(
+            @AuthenticationPrincipal User user,
+            @RequestPart(value = "info") PostDto.Request request,
+            @RequestPart(value = "file") MultipartFile multipartFile
+    ) {
+
+
+        postService.create(request, multipartFile);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{postId}")
+    public ResponseEntity<?> updatePost(@PathVariable("postId") Long postId, @RequestBody PostDto.UpdateRequest uRequest) {
+        postService.update(postId, uRequest);
+        return new ResponseEntity<>(HttpStatus.ACCEPTED);
+    }
+
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<?> deletePost(@PathVariable("postId") Long postId) {
+        postService.delete(postId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
