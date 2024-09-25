bc0416ca1dd311106bd14d0937fe17fe0638dea4
성철민 <tjdcjfals3@naver.com>
2024-09-25 17:57:18 +0900

    [FE][Feat] add LoginPage

---
+++ ifsavedog-BE/src/main/java/com/easteregg/ifsae/global/config/WebMvcConfig.java
@@ -0,0 +1,2 @@
+package com.easteregg.ifsae.global.config;public class WebMvcConfig {
+}
---
+++ ifsavedog-FE/src/routers/pathConstants.ts
@@ -0,0 +1,8 @@
+export const PATH = {
+  HOME: '/', // 메인 페이지
+  MAIN: '/main', // 메인 페이지
+  LANDING: '/landing', // 랜딩페이지
+  LOGIN: '/login', // 로그인 페이지
+  PASSWORD_RESET: '/password-reset', // 비밀번호 초기화 페이지
+  SIGNUP: '/signup', // 회원가입 페이지
+};
