e482a39855695eaaeb19ecaf1544b8d879f3d69a
천세경 <jay45652@gmail.com>
2024-09-27 17:49:43 +0900

    [BE] refactor : 사용하지않는 라이브러리

--- ifsavedog-BE/src/main/java/com/easteregg/ifsae/domain/post/controller/PostController.java
+++ ifsavedog-BE/src/main/java/com/easteregg/ifsae/domain/post/controller/PostController.java
@@ -3,16 +3,21 @@ package com.easteregg.ifsae.domain.post.controller;
 import com.easteregg.ifsae.domain.post.dto.PostDto;
 import com.easteregg.ifsae.domain.post.service.PostService;
 import com.easteregg.ifsae.domain.user.entity.User;
-import com.easteregg.ifsae.global.dto.CommonSuccessResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
