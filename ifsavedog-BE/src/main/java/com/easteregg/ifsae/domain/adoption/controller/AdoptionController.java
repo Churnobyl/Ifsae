f69050612a851b9fbcf40cbe69ad7450438d6956
kgc91747 <kgc91747@naver.com>
2024-09-27 10:27:39 +0900

    [BE]Feat: Adoption 등록, 수정, 삭제 구현
    - AdoptionController 일부 구현
       - 등록, 수정, 삭제(조회상태로 변경) 구현
       - 해당 API 테스트 완료
    
    [TODO]
    - Controller 구현
    - 조회 기능 구현

--- ifsavedog-BE/src/main/java/com/easteregg/ifsae/domain/adoption/controller/AdoptionController.java
+++ ifsavedog-BE/src/main/java/com/easteregg/ifsae/domain/adoption/controller/AdoptionController.java
@@ -1,8 +1,20 @@
 package com.easteregg.ifsae.domain.adoption.controller;
 
+import com.easteregg.ifsae.domain.adoption.dto.AdoptionCreateRequest;
+import com.easteregg.ifsae.domain.adoption.dto.AdoptionUpdateRequest;
+import com.easteregg.ifsae.domain.adoption.service.AdoptionService;
+import com.easteregg.ifsae.domain.user.entity.User;
+import com.easteregg.ifsae.global.dto.CommonSuccessResponse;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +24,29 @@ import org.springframework.web.bind.annotation.RestController;
 @Transactional
 @Slf4j
 public class AdoptionController {
+
+    private final AdoptionService adoptionService;
+
+    @PostMapping
+    public ResponseEntity<CommonSuccessResponse> applyAdoption(@AuthenticationPrincipal User user,
+                                                               @RequestBody AdoptionCreateRequest adoptionCreateRequest) {
+        adoptionService.createAdoption(user, adoptionCreateRequest);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @PutMapping("{adoptionId}")
+    public ResponseEntity<CommonSuccessResponse> updateAdoption(@AuthenticationPrincipal User user,
+                                                                @PathVariable Long adoptionId,
+                                                                @RequestBody AdoptionUpdateRequest adoptionUpdateRequest) {
+        adoptionService.updateAdoption(user, adoptionId, adoptionUpdateRequest);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @PutMapping("check/{adoptionId}")
+    public ResponseEntity<CommonSuccessResponse> checkAdoption(@AuthenticationPrincipal User user,
+                                                               @PathVariable Long adoptionId) {
+        adoptionService.checkAdoption(user, adoptionId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 
 }
