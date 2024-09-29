fc0037d60af2e9f26cf807d30c1daf532cef18d4
Churnobyl <tjdcjfals@gmail.com>
2024-09-29 20:52:44 +0900

    [FE][Feat] Routing changed

--- ifsavedog-FE/src/pages/LandingPage.tsx
+++ ifsavedog-FE/src/pages/LandingPage.tsx
@@ -1,16 +1,34 @@
+import LandingFirst from '@/components/landing/LandingFirst';
+import LandingLast from '@/components/landing/LandingLast';
+import LandingSecond from '@/components/landing/LandingSecond';
+import LandingThird from '@/components/landing/LandingThird';
+import { useEffect } from 'react';
+import { useCookies } from 'react-cookie';
 import { FullPage, Slide } from 'react-full-page';
 
 const LandingPage = () => {
+  const [, setCookie] = useCookies();
+
+  useEffect(() => {
+    setCookie('hasViewed', true, {
+      path: '/',
+      maxAge: 7 * 24 * 60 * 60,
+    });
+  }, [setCookie]);
+
   return (
     <FullPage>
       <Slide>
-        <span>1</span>
+        <LandingFirst />
       </Slide>
       <Slide>
-        <span>2</span>
+        <LandingSecond />
       </Slide>
       <Slide>
-        <span>3</span>
+        <LandingThird />
+      </Slide>
+      <Slide>
+        <LandingLast />
       </Slide>
     </FullPage>
   );
---
+++ ifsavedog-FE/src/pages/MainPage.tsx
@@ -0,0 +1,11 @@
+import MainLayout from '@/layouts/MainLayout';
+
+const MainPage = () => {
+  return (
+    <MainLayout showTopbar={true} showBottombar={true}>
+      <div>메인페이지</div>
+    </MainLayout>
+  );
+};
+
+export default MainPage;
