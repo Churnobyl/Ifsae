62d44194489744a902ac57c2f20be3f850b3bed0
성철민 <tjdcjfals3@naver.com>
2024-09-30 15:07:04 +0900

    [FE][Feat] Signup finished

---
+++ ifsavedog-FE/src/components/signup/RoleItem.tsx
@@ -0,0 +1,46 @@
+import { useSignupStore } from '@/stores/auth/signupStore';
+import classNames from 'classnames';
+import { useEffect, useState } from 'react';
+
+type RoleItemType = {
+  roleId: number;
+  roleName: string;
+  image: 'shelter' | 'individual';
+};
+
+const RoleItem = ({ roleId, roleName, image }: RoleItemType) => {
+  const signupStore = useSignupStore();
+  const [isSelected, setIsSelected] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (useSignupStore.getState().userInput.role === roleId) {
+      setIsSelected(true);
+    } else {
+      setIsSelected(false);
+    }
+  }, [roleId, useSignupStore.getState().userInput.role]);
+
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <div
+        className={classNames(
+          'w-24',
+          'h-24',
+          'bg-cover',
+          'hover:cursor-pointer',
+          {
+            'bg-shelter': image === 'shelter',
+            'bg-individual': image === 'individual',
+            'outline outline-4 outline-main': isSelected,
+          },
+        )}
+        onClick={() => {
+          signupStore.setUserInput({ role: roleId });
+        }}
+      ></div>
+      {roleName}
+    </div>
+  );
+};
+
+export default RoleItem;
