3e4470edb29a6e814f180d1bd79c8259a0ac469b
성철민 <tjdcjfals3@naver.com>
2024-10-02 12:31:05 +0900

    [Feat][Fix] remove import

--- ifsavedog-FE/src/components/donation/DogPreview.tsx
+++ ifsavedog-FE/src/components/donation/DogPreview.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-
 type DogPreviewType = {
   name: string;
   age: number;
   image: string;
-}
+};
 
 const DogPreview = ({ name, age, image }: DogPreviewType) => {
   return (
     <div className="bg-white rounded-lg p-2 shadow-md">
-      <img src={image} alt="강아지 사진" className="w-full h-32 object-cover rounded-lg" />
+      <img
+        src={image}
+        alt="강아지 사진"
+        className="w-full h-32 object-cover rounded-lg"
+      />
       <div className="mt-2 text-center">
         <p className="font-bold">{name}</p>
         <p>나이: {age}</p>
