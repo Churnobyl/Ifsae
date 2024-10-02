8f1ee0a4262fbfdacb961048edcd8fbc9fffe7cd
김재현 <wogus565@gmail.com>
2024-10-02 09:56:55 +0900

    fix: mongoDB find 안되던 것을 fix

--- ifsavedog-DATA/pythonProject/app/models/dog.py
+++ ifsavedog-DATA/pythonProject/app/models/dog.py
@@ -21,16 +21,14 @@ class PyObjectId(ObjectId):
         schema.update(type='string')
         return schema
 
-
-
 class Dog(Document):
+    desertionNo: str
     kindCd: str
     weightFloat: float
     koreanAge: int
-    desertionNo: str
 
     class Settings:
-        collection = "test"  # MongoDB의 컬렉션 이름 설정
+        name = "test"  # MongoDB의 컬렉션 이름 설정
 
 # Pydantic을 사용한 스키마 정의
 # Create하는 코드
@@ -42,10 +40,33 @@ class Dog(Document):
 
 class DogResponse(BaseModel):
     id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    desertionNo: str
     kindCd: str
     weightFloat: float
     koreanAge: int
-    desertionNo: str
+    
+    class Config:
+        json_encoders = {ObjectId: str}
+        from_attributes = True
+
+
+class User(Document):
+    user_id : str
+
+    class Settings:
+        collection = "user"  # MongoDB의 컬렉션 이름 설정
+
+# Pydantic을 사용한 스키마 정의
+# Create하는 코드
+# class DogCreate(BaseModel):
+#     name: str
+#     breed: str
+#     age: Optional[int]
+#     description: Optional[str]
+
+class UserResponse(BaseModel):
+    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+    user_id : str
     
     class Config:
         json_encoders = {ObjectId: str}
