5ad34bf4b03c419f05f2bdb64a590f731ad22824
Churnobyl <tjdcjfals@gmail.com>
2024-10-03 14:54:05 +0900

    [FE][Fix] forCheck

--- ifsavedog-FE/env.sh
+++ ifsavedog-FE/env.sh
@@ -8,9 +8,9 @@ echo "VITE_COOKIE_MAX_AGE: $VITE_COOKIE_MAX_AGE"
 
 # Create env-config.js file
 echo "window._env_ = {" > ./dist/env-config.js
-echo "  VITE_BASE_URL: '\"${VITE_BASE_URL}\"'," >> ./dist/env-config.js
+echo "  VITE_BASE_URL: '${VITE_BASE_URL}'," >> ./dist/env-config.js
 echo "  VITE_AXIOS_TIMEOUT: ${VITE_AXIOS_TIMEOUT}," >> ./dist/env-config.js
-echo "  VITE_COOKIE_NAME_FOR_LANDING_PAGE: '\"${VITE_COOKIE_NAME_FOR_LANDING_PAGE}\"'," >> ./dist/env-config.js
+echo "  VITE_COOKIE_NAME_FOR_LANDING_PAGE: '${VITE_COOKIE_NAME_FOR_LANDING_PAGE}'," >> ./dist/env-config.js
 echo "  VITE_COOKIE_MAX_AGE: ${VITE_COOKIE_MAX_AGE}" >> ./dist/env-config.js
 echo "};" >> ./dist/env-config.js
 
