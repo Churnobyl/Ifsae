dfdf887023f310309af0b01e6c692bcefecce6d8
김재현 <wogus565@gmail.com>
2024-10-04 10:25:12 +0900

    [feat] 스케쥴 작업 실행 파일

---
+++ ifsavedog-DATA/pythonProject/scheduler/scheduler.py
@@ -0,0 +1,18 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.triggers.cron import CronTrigger
+from datetime import datetime
+from scheduler.inputMongo import input_data_to_mongo
+from scheduler.calculate_character import calculate_character_score
+import time
+
+scheduler = BlockingScheduler()
+scheduler.add_job(input_data_to_mongo, CronTrigger(hour=1, minute=0))
+scheduler.add_job(calculate_character_score, CronTrigger(hour=1, minute=0))
+scheduler.start()
+
+try:
+    while True:
+        time.sleep(2)
+        print("Main Thread is running")
+except (KeyboardInterrupt, SystemExit):
+  scheduler.shutdown()
