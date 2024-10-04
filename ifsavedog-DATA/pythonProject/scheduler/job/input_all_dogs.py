8caa6494b6ae013128e266986dd687c1c3646e9e
김재현 <wogus565@gmail.com>
2024-10-04 21:51:03 +0900

    [DATA][init] dog data를 db에 넣는 job 생성

---
+++ ifsavedog-DATA/pythonProject/scheduler/job/input_all_dogs.py
@@ -0,0 +1,13 @@
+from service.inputMongo import *
+from db.maria import *
+import pandas as pd
+
+
+def input_all_dogs():
+  db = next(get_db())
+  df = preprocess_data(get_data())
+  if is_exist_data(db, df):
+    print("exist data")
+    return
+  else:
+    return insert_data_to_sql(db, df)
