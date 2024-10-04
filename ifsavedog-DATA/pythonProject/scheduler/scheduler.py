643b93c212ceb005c0b98132c56092040151de3a
김재현 <wogus565@gmail.com>
2024-10-04 10:27:44 +0900

    [fix] background에서 실행되도록 수정

--- ifsavedog-DATA/pythonProject/scheduler/scheduler.py
+++ ifsavedog-DATA/pythonProject/scheduler/scheduler.py
@@ -1,11 +1,11 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.blocking import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 from datetime import datetime
 from scheduler.inputMongo import input_data_to_mongo
 from scheduler.calculate_character import calculate_character_score
 import time
 
-scheduler = BlockingScheduler()
+scheduler = BackgroundScheduler()
 scheduler.add_job(input_data_to_mongo, CronTrigger(hour=1, minute=0))
 scheduler.add_job(calculate_character_score, CronTrigger(hour=1, minute=0))
 scheduler.start()
