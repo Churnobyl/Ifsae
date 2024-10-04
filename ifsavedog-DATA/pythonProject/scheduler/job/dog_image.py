b640d511935cd3e405ebacf7958f348958c14052
SungyoonRyu <rara0620@naver.com>
2024-10-05 00:16:42 +0900

    [DATA]Feat : add image download job by date

--- ifsavedog-DATA/pythonProject/scheduler/job/dog_image.py
+++ ifsavedog-DATA/pythonProject/scheduler/job/dog_image.py
@@ -1,5 +1,9 @@
 import re
 import os
 
+from service.image_download import *
+
 def download_dog_image_by_date(date):
-    pass
+    dog_list = get_dog_list_by_date(date)
+    
+    download_image_dog_list(dog_list)
