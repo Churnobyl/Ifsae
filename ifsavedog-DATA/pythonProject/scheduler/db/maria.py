f8635b0562052248d45d167d82adbba736bc90a6
김재현 <wogus565@gmail.com>
2024-10-04 23:39:30 +0900

    [DATA][feat] sql 재연결코드 추가

--- ifsavedog-DATA/pythonProject/scheduler/db/maria.py
+++ ifsavedog-DATA/pythonProject/scheduler/db/maria.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from settings import config
 
-engine = create_engine(config.DATABASE_URL)
+engine = create_engine(config.DATABASE_URL, pool_recycle=3600)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 def get_db():
