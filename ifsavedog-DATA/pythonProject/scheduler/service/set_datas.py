542ce066d7273dbbb66c8b6fa35066f2a4fc2857
SungyoonRyu <rara0620@naver.com>
2024-10-04 23:22:24 +0900

    [DATA]Feat : add dog image vector data access method

--- ifsavedog-DATA/pythonProject/scheduler/service/set_datas.py
+++ ifsavedog-DATA/pythonProject/scheduler/service/set_datas.py
@@ -3,6 +3,9 @@ from db.maria import get_db
 
 from models.rank import Rank
 
+def insert_dog_image_vector(dog_vector_list):
+    get_mongo_db().dog_image_vector.insert_many(dog_vector_list)
+
 def update_user_prefer_vector(user_id, prefer_rank_list):
     try:
         get_mongo_db().user_image_vector_rank.update(
@@ -18,4 +21,6 @@ def update_user_prefer_vector(user_id, prefer_rank_list):
 def update_user_recommendation_rank(user_id, rank_list):
     for ranking in rank_list:
         pass
-    next(get_db()).query(Rank).update().where(Rank.user_id == user_id).values()
+    session = next(get_db())
+    session.query(Rank).update().where(Rank.user_id == user_id).values()
+    session.commit()
