ac3ebd74b8040f29d7f284c1e398386760dd809d
천세경 <jay45652@gmail.com>
2024-10-05 20:54:02 +0900

    [FE][Feat] add EditProfileForm

--- ifsavedog-FE/src/components/video/VideoList.tsx
+++ ifsavedog-FE/src/components/video/VideoList.tsx
@@ -10,7 +10,7 @@ const VideoList = () => {
   });
 
   return (
-    <div className="grid grid-cols-2 gap-4 p-4">
+    <div className="w-full p-4 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
       {videoData.map((video) => (
         <VideoCard
           videoId={video.id}
--- ifsavedog-FE/src/pages/mypages/UserLikeVideo.tsx
+++ ifsavedog-FE/src/pages/mypages/UserLikeVideo.tsx
@@ -3,13 +3,13 @@ import MainLayout from '@/layouts/MainLayout';
 const UserLikeVideo = () => {
   return (
     <MainLayout showTopbar={true} showBottombar={true}>
-      <div className="w-full flex flex-col items-center">
-        <div className="w-10/12">
+      <div className="relative w-full flex flex-col items-center">
+        <div className="w-10/12 overflow-auto">
           <div className="flex flex-row justify-start items-center">
-            <div className="py-2 mr-1 text-black font-semibold text-[24px]">
+            <div className="py-2 mr-1 text-black font-semibold text-2xl">
               내가 좋아요 한 영상
             </div>
-            <div className="py-3 text-black text-[18px] self-end">13</div>
+            <div className="py-3 text-black text-lg self-end">13</div>
           </div>
 
           <div>
