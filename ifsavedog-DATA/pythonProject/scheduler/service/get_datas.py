06773c798f8500ae7b2fcc51f917145ef9dc3d5c
김재현 <wogus565@gmail.com>
2024-10-05 16:45:32 +0900

    [DATA] feat : character data가져오는 함수 추가

--- ifsavedog-DATA/pythonProject/scheduler/service/get_datas.py
+++ ifsavedog-DATA/pythonProject/scheduler/service/get_datas.py
@@ -1,7 +1,7 @@
 from db.mongo import get_mongo_db
 from db.maria import get_db
 
-from models.user import User
+from models.user import User, UserSurvey
 from models.rating import Rating
 
 def get_all_users():
@@ -22,3 +22,10 @@ def get_user_prefer_image_vector(user_id):
 
 def get_dog_image_vector(desertionNo):
     return get_mongo_db().dog_image_vector.find_one({"desertionNo": desertionNo})
+
+def get_dog_vector_data():
+    db = get_mongo_db()
+    return db.dog_character.find()
+
+def get_user_character_survey(user_id):
+    return next(get_db()).query(UserSurvey).filter(UserSurvey.user_id == user_id).first()
