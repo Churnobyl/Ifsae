7561d4b217dffe4084a4006937a486efeae166c4
SungyoonRyu <rara0620@naver.com>
2024-10-05 15:08:59 +0900

    [DATA]Feat : add ranking by image vector module

--- ifsavedog-DATA/pythonProject/scheduler/job/image_vector_filter.py
+++ ifsavedog-DATA/pythonProject/scheduler/job/image_vector_filter.py
@@ -1,10 +1,12 @@
 from service.get_datas import *
 from service.user_preference import *
+from service.insert_values import *
 
 def set_preference_image_vector():
     users = get_all_users()
     for user in users:
         pass
 
-def rank_image_vector(): #이름 바꿔야함
-    pass
+def rank_image_vector(id): #이름 바꿔야함, 파라미터 없애기
+    rank_list = calc_cos_similarity(id)
+    insert_image_vector_rank(29, rank_list)
