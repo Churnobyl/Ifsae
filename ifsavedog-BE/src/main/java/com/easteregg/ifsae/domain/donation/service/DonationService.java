7dedde1e47a609dfce5d24613be93cb26e71e046
서경덕 <skdb0105@naver.com>
2024-10-05 19:54:29 +0900

    [BE]feat : 기부 서비스 구현

---
+++ ifsavedog-BE/src/main/java/com/easteregg/ifsae/domain/donation/service/DonationService.java
@@ -0,0 +1,13 @@
+package com.easteregg.ifsae.domain.donation.service;
+
+import com.easteregg.ifsae.domain.donation.dto.DonationDto;
+import com.easteregg.ifsae.domain.user.entity.User;
+
+import java.util.List;
+
+
+public interface DonationService {
+    void createDonation(User user, DonationDto.createDonationRequest request, Long dogId);
+    List<DonationDto.getUserDonatedListResponse> getUserDonatedList(User user);
+    List<DonationDto.getShelterDonorListResponse> getShelterDonorList(User user);
+}
