7a2c3724f916c2513bdc1bd345304c819ae4426d
kgc91747 <kgc91747@naver.com>
2024-10-05 22:29:33 +0900

    fix: conflict solve

--- ifsavedog-FE/src/pages/AdoptionPage.tsx
+++ ifsavedog-FE/src/pages/AdoptionPage.tsx
@@ -4,17 +4,7 @@ import DogPreview from '@/components/common/DogPreviewBox';
 import TestImage from '@/assets/logo.webp';
 import LeftArrow from '@/assets/icon/scroll-arrow-left.svg';
 import RightArrow from '@/assets/icon/scroll-arrow-right.svg';
-<<<<<<< HEAD
-
-interface Dog {
-  id: number;
-  name: string;
-  age: number;
-  image: string;
-}
-=======
 import { DogType } from '@/types/dog/DogType';
->>>>>>> ead8e1968aa7094bc36a3dc47406c9e3fbc3a897
 
 interface DonationInfo {
   name: string;
@@ -35,11 +25,7 @@ const AdoptionPage = () => {
     },
   ];
 
-<<<<<<< HEAD
-  const recommenedList: Dog[] = [
-=======
   const recommenedList: DogType[] = [
->>>>>>> ead8e1968aa7094bc36a3dc47406c9e3fbc3a897
     { id: 1, name: '초코', age: 1, image: TestImage },
     { id: 2, name: '코코', age: 2, image: TestImage },
     { id: 3, name: '보리', age: 3, image: TestImage },
@@ -47,11 +33,7 @@ const AdoptionPage = () => {
     { id: 5, name: '말티', age: 5, image: TestImage },
   ];
 
-<<<<<<< HEAD
-  const followList: Dog[] = [
-=======
   const followList: DogType[] = [
->>>>>>> ead8e1968aa7094bc36a3dc47406c9e3fbc3a897
     { id: 1, name: '초코', age: 1, image: TestImage },
     { id: 2, name: '코코', age: 2, image: TestImage },
     { id: 3, name: '보리', age: 3, image: TestImage },
