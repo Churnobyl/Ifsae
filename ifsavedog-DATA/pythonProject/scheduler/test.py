8960d0479ab3d92efd28f2e54445c676bfb839b1
김재현 <wogus565@gmail.com>
2024-10-05 16:53:02 +0900

    [DATA] Test : test코드 수정

--- ifsavedog-DATA/pythonProject/scheduler/test.py
+++ ifsavedog-DATA/pythonProject/scheduler/test.py
@@ -1,7 +1,7 @@
 from db.maria import get_db
 from service.get_datas import get_all_users
-from job.input_all_dogs import *
+from job.recommend_by_character import calculate_dog_character_score
 
-input_all_dogs()
+calculate_dog_character_score()
 
 # print(get_all_users())
