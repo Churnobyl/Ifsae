dfbf01f268f0ab6af12d7f99d346b87bf43094a2
SungyoonRyu <rara0620@naver.com>
2024-10-06 15:35:11 +0900

    [DATA]Feat : add latest filter to recommendation service

--- ifsavedog-DATA/pythonProject/scheduler/service/recommendation.py
+++ ifsavedog-DATA/pythonProject/scheduler/service/recommendation.py
@@ -1,13 +1,25 @@
 from service.get_datas import *
 
-from service.set_datas import update_user_recommendation_rank
+from service.set_datas import upsert_user_recommendation_rank
 
-def insert_recommendation_rank(user_id):
+def insert_recommendation_rank(user_id, index=0,image_vector_rank_=None):
+    
+    liked_num = len(get_user_rating(user_id))
     # TODO: modify parameters
     ws = []
     
-    image_vector_rank = get_image_vector_rank(user_id=user_id)
-    character_rank = get_character_rank(user_id=user_id)
+    image_vector_rank = []
+    if image_vector_rank_ == None and liked_num > 0:
+        image_vector_rank = get_image_vector_rank(user_id=user_id)
+    else:
+        image_vector_rank = image_vector_rank_
+        
     
+    # character_rank = get_character_rank(user_id=user_id)
+    latest_rank = get_latest_dog_rank()['rank_list']
     result_list = []
-    update_user_recommendation_rank()
+    
+    # result 넣기
+    result_list.extend(latest_rank[:100])
+    # print(result_list)
+    upsert_user_recommendation_rank(user_id=user_id, rank_list=result_list)
