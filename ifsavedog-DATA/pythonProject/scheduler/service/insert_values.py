77b51e4c92369465eec122d8e40d856ed3010465
SungyoonRyu <rara0620@naver.com>
2024-10-06 15:34:00 +0900

    [DATA]Feat : implement function for upsert ranking table

--- ifsavedog-DATA/pythonProject/scheduler/service/insert_values.py
+++ ifsavedog-DATA/pythonProject/scheduler/service/insert_values.py
@@ -4,3 +4,7 @@ def insert_image_vector_rank(user_id, rank_sorted_list):
     rank_list = [{"dog_id": i[0], "desertion_no": i[1], "similarity": i[2].item()} for i in rank_sorted_list]
     print(rank_list)
     update_user_prefer_vector(user_id, rank_list)
+
+def insert_latest_rank(latest_list):
+    rank_list = [{"dog_id": i.id, "desertion_no": i.desertion_no, "happen_dt": i.happen_dt} for i in latest_list]
+    update_latest_dog_rank(rank_list)
