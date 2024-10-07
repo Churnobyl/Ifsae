d43de07dd4a89d92f2433b098b1b0a40e0393a4a
김재현 <wogus565@gmail.com>
2024-10-07 11:00:01 +0900

    [DATA] Refactor : insert_dog 작업 모듈화

--- ifsavedog-DATA/pythonProject/scheduler/job/input_all_dogs.py
+++ ifsavedog-DATA/pythonProject/scheduler/job/input_all_dogs.py
@@ -1,6 +1,7 @@
 from service.insert_dog_datas import *
 from db.maria import *
-def input_all_dogs():
+def input_all_dogs(start_date, end_date):
   db = next(get_db())
-  df = preprocess_data(get_data())  
+  delete_data()
+  df = preprocess_data(get_data(start_date, end_date))  
   return insert_data_to_sql(db, df)
