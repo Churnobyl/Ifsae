b0985629bee5fe0f3756c98d3f2dde9077946529
SungyoonRyu <rara0620@naver.com>
2024-10-07 09:50:20 +0900

    [DATA]Fix : modify recommendation logic
    integrate 4 algorithm

--- ifsavedog-DATA/pythonProject/scheduler/service/recommendation.py
+++ ifsavedog-DATA/pythonProject/scheduler/service/recommendation.py
@@ -6,7 +6,7 @@ def insert_recommendation_rank(user_id, index=0,image_vector_rank_=None):
     
     liked_num = len(get_user_rating(user_id))
     # TODO: modify parameters
-    ws = []
+    ws = [20, 20, 59, 1]
     
     image_vector_rank = []
     if image_vector_rank_ == None and liked_num > 0:
@@ -15,11 +15,15 @@ def insert_recommendation_rank(user_id, index=0,image_vector_rank_=None):
         image_vector_rank = image_vector_rank_
         
     
-    # character_rank = get_character_rank(user_id=user_id)
+    character_rank = get_character_rank(user_id=user_id)
     latest_rank = get_latest_dog_rank()['rank_list']
+    liked_rank = get_liked_dog_rank()['rank_list']
     result_list = []
     
     # result 넣기
-    result_list.extend(latest_rank[:100])
+    result_list.extend(image_vector_rank[index*ws[0]:(index+1)*ws[0]])
+    result_list.extend(character_rank[index*ws[1]:(index+1)*ws[1]])
+    result_list.extend(latest_rank[index*ws[2]:(index+1)*ws[2]])
+    result_list.extend(liked_rank[index*ws[3]:(index+1)*ws[3]])
     # print(result_list)
     upsert_user_recommendation_rank(user_id=user_id, rank_list=result_list)
