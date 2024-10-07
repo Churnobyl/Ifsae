8e6410f1dd8921e6670b80de9cd11ec84cbd6cc2
천세경 <jay45652@gmail.com>
2024-10-07 18:03:53 +0900

    [FE]feat : centerPage

--- ifsavedog-FE/src/components/adoption/AdoptionCard.tsx
+++ ifsavedog-FE/src/components/adoption/AdoptionCard.tsx
@@ -10,30 +10,32 @@ interface CardType {
 
 const AdoptionCard = ({ dog, duration, category }: CardType) => {
   const { name, age, image } = dog;
-  const bgColor = category === 'donation' ? 'bg-lightGray' : 'bg-lightBlue';
-  const borderColor = category === 'donation' ? 'bg-lightGray' : 'bg-lightBlue';
-  const textColor = category === 'donation' ? 'bg-lightGray' : 'bg-lightBlue';
-  const subTextColor =
-    category === 'donation' ? 'bg-lightGray' : 'bg-lightBlue';
+  const bgColor = category === 'donation' ? 'bg-main' : 'bg-pointYellow';
+  const borderColor = category === 'donation' ? 'bg-main' : 'bg-pointYellow';
+  const textColor = category === 'donation' ? 'bg-main' : 'bg-pointYellow';
+  const subTextColor = category === 'donation' ? 'bg-main' : 'bg-pointYellow';
   return (
-    <div
-      className={classNames(
-        'rounded-lg shadow-md p-6 h-32 flex items-center',
-        bgColor,
-        borderColor,
-      )}
-    >
-      <div className="flex items-center space-x-6">
-        <img
-          src={image}
-          alt={name}
-          className="w-20 h-20 rounded-full border-2 border-white shadow-md"
-        />
-        <div>
+    <div className="w-full flex items-center justify-center">
+      <div
+        className={classNames(
+          'w-11/12 rounded-lg shadow-md p-2 flex flex-row items-center justify-between',
+          bgColor,
+          borderColor,
+        )}
+      >
+        <div className="min-w-36 p-4 text-black">
+          <div className="my-1 font-bold text-xl ">
+            {category === 'donation' ? '후원 카드' : '입양 카드'}
+          </div>
           <h3 className={`text-lg font-bold ${textColor}`}>{name}</h3>
           <p className={`text-sm ${subTextColor}`}>나이: {age}살</p>
           <p className={`text-sm ${subTextColor}`}>함께한 기간: {duration}일</p>
         </div>
+        <img
+          src={image}
+          alt={name}
+          className="w-32 h-36 m-2 object-cover overflow-hidden"
+        />
       </div>
     </div>
   );
