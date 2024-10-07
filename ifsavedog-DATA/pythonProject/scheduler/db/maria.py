c1ffd42772529eb858318fda6321f66d98050641
SungyoonRyu <rara0620@naver.com>
2024-10-07 09:46:48 +0900

    [DATA]Fix : modify data access using contextmanager
    modify maria db access module

--- ifsavedog-DATA/pythonProject/scheduler/db/maria.py
+++ ifsavedog-DATA/pythonProject/scheduler/db/maria.py
@@ -1,10 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from settings import config
+from contextlib import contextmanager
 
 engine = create_engine(config.DATABASE_URL, pool_recycle=3600)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
+@contextmanager
 def get_db():
     db = SessionLocal()
     try:
