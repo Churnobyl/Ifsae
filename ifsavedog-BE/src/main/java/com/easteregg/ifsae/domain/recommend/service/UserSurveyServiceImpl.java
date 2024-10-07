a6e40061c883b4e2b33af2c8753db164bc4061d0
김재현 <wogus565@gmail.com>
2024-10-07 09:42:21 +0900

    [BE] feat : FASTAPI 전달 메서드 추가

--- ifsavedog-BE/src/main/java/com/easteregg/ifsae/domain/recommend/service/UserSurveyServiceImpl.java
+++ ifsavedog-BE/src/main/java/com/easteregg/ifsae/domain/recommend/service/UserSurveyServiceImpl.java
@@ -7,7 +7,14 @@ import com.easteregg.ifsae.domain.user.entity.User;
 import com.easteregg.ifsae.domain.user.repository.UserRepository;
 import java.util.NoSuchElementException;
 import lombok.RequiredArgsConstructor;
+import org.apache.http.protocol.HTTP;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.http.HttpHeaders;
 
 @Service
 @RequiredArgsConstructor
@@ -17,6 +24,18 @@ public class UserSurveyServiceImpl implements UserSurveyService {
 
     private final UserRepository userRepository;
 
+    @Value("${fastApi.endpoint}")
+    String fastApiEndpoint;
+    public void sendAPI(User user){
+        RestTemplate restTemplate = new RestTemplate();
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        HttpEntity<Long> requestEntity = new HttpEntity<>(user.getId(), headers);
+        restTemplate.exchange(fastApiEndpoint, HttpMethod.POST, requestEntity, User.class);
+    }
+
     @Override
     public void createUserSurvey(User user, UserSurveyRequest userSurveyRequest) {
         UserSurvey userSurvey = UserSurvey.from(user, userSurveyRequest);
@@ -24,6 +43,7 @@ public class UserSurveyServiceImpl implements UserSurveyService {
 
         userSurveyRepository.save(userSurvey);
         userRepository.save(user);
+        sendAPI(user);
     }
 
     @Override
