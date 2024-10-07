3af18de60896c34e5b6941c35d74b528502da94e
SungyoonRyu <rara0620@naver.com>
2024-10-07 09:53:11 +0900

    [DATA]Feat : add sequential job daily

--- ifsavedog-DATA/pythonProject/scheduler/job/sequential.py
+++ ifsavedog-DATA/pythonProject/scheduler/job/sequential.py
@@ -1,9 +1,21 @@
-from job.dog_image import *
 import datetime
 
-def sequential_job_every_day():
-    today = datetime.date.today().strftime('%Y%m%d')
-    download_dog_image_by_date(today)
+from job.dog_image import *
+from service.latest_dog import rank_latest_dog
+from job.image_vector_filter import rank_image_vector
 
-def sequential_job():
+def sequential_job_for_new_data():
     today = datetime.date.today().strftime('%Y%m%d')
+    
+    # 1. 강아지 api 쏘고 mariaDB에 삽입
+    # 2. 추가된 정보 참고해서 image download
+    # 3. 다운로드 된 강아지 image 모델 이용해서 vector 뽑기
+    # 4. 유저 별 + 기준 별 개인화 추천 랭크 최신화
+    #     - image_vector_rank
+    #     - character_rank
+    download_dog_image_by_date(today)
+    rank_latest_dog()
+
+def sequential_job_daily():
+    today = datetime.date.today().strftime('%Y%m%d')
+    rank_image_vector()
