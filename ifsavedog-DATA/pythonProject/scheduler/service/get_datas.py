34252b07090bd321e21d6e62223022c01488ff54
SungyoonRyu <rara0620@naver.com>
2024-10-07 09:46:48 +0900

    [DATA]Fix : modify data access using contextmanager
    modify maria db access module

--- ifsavedog-DATA/pythonProject/scheduler/service/get_datas.py
+++ ifsavedog-DATA/pythonProject/scheduler/service/get_datas.py
@@ -6,28 +6,38 @@ from models.user import User, UserSurvey
 from models.rating import Rating
 
 def get_all_users():
-    return next(get_db()).query(User).all()
+    with get_db() as db:
+        return db.query(User).all()
 
 def get_all_image_vector():
     return get_mongo_db().dog_image_vector.find().to_list()
 
+def get_all_rating():
+    with get_db() as db:
+        return db.query(Rating).all()
+
 def get_user_rating(user_id):
-    return next(get_db()).query(Rating).filter(Rating.user_id == user_id).all()
+    with get_db() as db:
+        return db.query(Rating).filter(Rating.user_id == user_id).all()
 
 def get_dog_image_vectors_list(dog_list):
     return get_mongo_db().dog_image_vector.find(dog_list).to_list()
 
 def get_dogs_by_date(date):
-    return next(get_db()).query(Dog).filter(Dog.happen_dt == date).all()
+    with get_db() as db:
+        return db.query(Dog).filter(Dog.happen_dt == date).all()
 
 def get_dogs_by_date_list(date_list):
-    return next(get_db()).query(Dog).filter(Dog.happen_dt.in_(date_list)).all()
+    with get_db() as db:
+        return db.query(Dog).filter(Dog.happen_dt.in_(date_list)).all()
 
 def get_dogs_ordered_by_date(num):
-    return next(get_db()).query(Dog).order_by(Dog.happen_dt).limit(num).all()
+    with get_db() as db:
+        return db.query(Dog).order_by(Dog.happen_dt).limit(num).all()
 
 def get_dogs_boot(strform):
-    return next(get_db()).query(Dog).filter(Dog.happen_dt.contains(strform)).all()
+    with get_db() as db:
+        return db.query(Dog).filter(Dog.happen_dt.contains(strform)).all()
 
 def get_user_prefer_image_vector(user_id):
     return get_mongo_db().user_image_vector.find_one({"user_id": user_id})
@@ -40,7 +50,8 @@ def get_dog_vector_data():
     return db.dog_character.find()
 
 def get_user_character_survey(user_id):
-    return next(get_db()).query(UserSurvey).filter(UserSurvey.user_id == user_id).first()
+    with get_db() as db:
+        return db.query(UserSurvey).filter(UserSurvey.user_id == user_id).first()
 
 def get_image_vector_rank(user_id):
     return get_mongo_db().user_image_vector_rank.find_one({"user_id": user_id})
@@ -50,3 +61,7 @@ def get_character_rank(user_id):
 
 def get_latest_dog_rank():
     return get_mongo_db().latest_dog_rank.find_one({})
+
+def get_liked_dog_rank():
+    return get_mongo_db().liked_dog_rank.find_one({})
+    
