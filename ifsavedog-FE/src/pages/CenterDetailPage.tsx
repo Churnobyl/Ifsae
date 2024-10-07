3516156b1ae134edfe371ae96fc351afcffb55d4
천세경 <jay45652@gmail.com>
2024-10-07 14:17:28 +0900

    [FE]feat : ShelterDetailType 빌드 오류 수정

--- ifsavedog-FE/src/pages/CenterDetailPage.tsx
+++ ifsavedog-FE/src/pages/CenterDetailPage.tsx
@@ -2,7 +2,7 @@ import { useParams } from 'react-router-dom';
 
 const CenterDetailPage = () => {
   const { id } = useParams<{ id: string }>();
-
+  console.log(id);
   return <div>센터 페이지</div>;
 };
 export default CenterDetailPage;
