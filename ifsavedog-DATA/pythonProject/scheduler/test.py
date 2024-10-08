77ff9f40f6b379e6f4f1ad99f67ad306f80f583a
SungyoonRyu <rara0620@naver.com>
2024-10-08 17:51:31 +0900

    [DATA]Test : modify test case

--- ifsavedog-DATA/pythonProject/scheduler/test.py
+++ ifsavedog-DATA/pythonProject/scheduler/test.py
@@ -1,9 +1,9 @@
 from service.get_datas import *
-from service.dog_image_vector import *
+from service.liked_dog import *
 
 image_name = 'KakaoTalk_20241007_134037647.jpg'
 # make_movie(image_name)
 # print(get_all_users())
 # download_dog_image_boot('2024070')
 # print(len(get_all_image_vector()))
-print(ttttt())
+rank_liked_dog()
