a449f75e090e25fa083e8267f4f8eab241bab54e
SungyoonRyu <rara0620@naver.com>
2024-10-08 17:53:22 +0900

    [DATA]Fix : modify list paramenter value by id

--- ifsavedog-DATA/pythonProject/scheduler/job/image_vector_filter.py
+++ ifsavedog-DATA/pythonProject/scheduler/job/image_vector_filter.py
@@ -12,12 +12,11 @@ def rank_image_vector_by_user(id): #이름 바꿔야함, 파라미터 없애기
     print('got rank_list')
     insert_image_vector_rank(id, rank_list)
     
-def rank_image_vector():
-    user_list = get_all_users()
+def rank_image_vector(user_ids_list):
     dog_image_vectors = get_all_image_vector()
     
-    for user in user_list:
-        rank_list = rank_cos_similarity(user.id, dog_image_vectors)
+    for user_id in user_ids_list:
+        rank_list = rank_cos_similarity(user_id, dog_image_vectors)
         if rank_list == None:
             continue
-        insert_image_vector_rank(user.id, rank_list)
+        insert_image_vector_rank(user_id, rank_list)
