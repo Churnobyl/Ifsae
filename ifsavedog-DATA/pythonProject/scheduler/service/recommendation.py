25a74c62ce47afaadbdc8fd98a7f1ddceaf26968
SungyoonRyu <rara0620@naver.com>
2024-10-08 17:56:12 +0900

    [DATA]Feat : add parameterizing proportion of rank

--- ifsavedog-DATA/pythonProject/scheduler/service/recommendation.py
+++ ifsavedog-DATA/pythonProject/scheduler/service/recommendation.py
@@ -1,12 +1,45 @@
 from service.get_datas import *
 
 from service.set_datas import upsert_user_recommendation_rank
+from service.parameters import *
+
+config = {
+    "algorithm": [
+        {
+            "max": 10,
+            "start": 80,
+            "steepness": 0.2,
+        },
+        {
+            "max": 20,
+            "start": 80,
+            "steepness": 0.18,
+        },
+        {
+            "max": 50,
+            "start": 0,
+            "steepness": 0.08,
+        },
+        {
+            "max": 45,
+            "start": 0,
+            "steepness": 0.05,
+        },
+        {
+            "max": 60,
+            "start": 0,
+            "steepness": 0.06,
+        }
+    ]
+}
 
 def insert_recommendation_rank(user_id, index=0,image_vector_rank_=None):
-    
+    global config
     liked_num = len(get_user_rating(user_id))
     # TODO: modify parameters
-    ws = [20, 20, 59, 1]
+    ws = proportion(get_parameters(liked_num, config=config)[:4])*100
+    if len(ws) < 100:
+        ws[0] += (100 - len(ws))
     
     image_vector_rank = []
     if image_vector_rank_ == None and liked_num > 0:
@@ -18,12 +51,20 @@ def insert_recommendation_rank(user_id, index=0,image_vector_rank_=None):
     character_rank = get_character_rank(user_id=user_id)
     latest_rank = get_latest_dog_rank()['rank_list']
     liked_rank = get_liked_dog_rank()['rank_list']
+    liked_rank_num = len(liked_rank)
     result_list = []
     
     # result 넣기
-    result_list.extend(image_vector_rank[index*ws[0]:(index+1)*ws[0]])
-    result_list.extend(character_rank[index*ws[1]:(index+1)*ws[1]])
-    result_list.extend(latest_rank[index*ws[2]:(index+1)*ws[2]])
-    result_list.extend(liked_rank[index*ws[3]:(index+1)*ws[3]])
-    # print(result_list)
-    upsert_user_recommendation_rank(user_id=user_id, rank_list=result_list)
+    result_list.extend(latest_rank[index*ws[0]:(index+1)*ws[0]])
+    result_list.extend(liked_rank[index*ws[1]:(index+1)*ws[1]])
+    result_list.extend(image_vector_rank[index*ws[2]:(index+1)*ws[2]])
+    result_list.extend(character_rank[index*ws[3]:(index+1)*ws[3]])
+    
+    if len(result_list) != 100:
+        raise ValueError('not length 100')
+    print(result_list)
+    # upsert_user_recommendation_rank(user_id=user_id, rank_list=result_list)
+
+def recommedate(user_id_list):
+    for user_id in user_id_list:
+        insert_recommendation_rank(user_id=user_id)
