b6895fee71e4747ae30866389792894dd718e4a6
SungyoonRyu <rara0620@naver.com>
2024-10-08 17:52:09 +0900

    [DATA]Feat : implement liked dog rank

--- ifsavedog-DATA/pythonProject/scheduler/service/liked_dog.py
+++ ifsavedog-DATA/pythonProject/scheduler/service/liked_dog.py
@@ -1,11 +1,22 @@
 import pandas as pd
 
 from service.get_datas import *
+from service.insert_rank_values import insert_liked_rank
 
 def rank_liked_dog():
     rating_list = get_all_rating()
-    rating_df = pd.DataFrame([{"user_id": rate.user_id, "dog_id": rate.dog_id, "rating": rate.rating, "create_at": rate.created_at} for rate in rating_list])
-    count_result = list(rating_df['dog_id'].value_counts().keys())
+    # for d,a in rating_list:
+    #     print(d, a)
+    rating_df = pd.DataFrame([{"user_id": rate.user_id, "dog_id": rate.dog_id, "desertion_no": dog.desertion_no, "rating": rate.rating, "create_at": rate.created_at} for rate, dog in rating_list])
+    # print(rating_df)
+    rating_count_df = rating_df['dog_id'].value_counts().reset_index()
+    # print(rating_count_df)
+    rating_merged_df = pd.merge(rating_count_df, rating_df[['dog_id', 'desertion_no']], how='left').drop_duplicates(subset=['dog_id'])
+    # print(rating_merged_df)
+    count_result = rating_merged_df.to_records().tolist()
     print(count_result)
-    
-    # print(rating_list)
+
+    insert_liked_rank(count_result)
+
+def get_rating_day_ago(day):
+    pass
