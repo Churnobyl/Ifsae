4f640ffb1a6d4a84187700850a3249ad9fbe7b84
SungyoonRyu <rara0620@naver.com>
2024-10-10 08:45:10 +0900

    [DATA]Fix : fix param 0 error

--- ifsavedog-DATA/pythonProject/scheduler/service/recommendation.py
+++ ifsavedog-DATA/pythonProject/scheduler/service/recommendation.py
@@ -38,7 +38,8 @@ def insert_recommendation_rank(user_id, index=0,image_vector_rank_=None):
     liked_num = len(get_user_rating(user_id))
     # TODO: modify parameters
     ws = np.int32(np.round(proportion(get_parameters(liked_num, config=config)[:4])*100))
-    print(ws)
+    print(user_id)
+    print(ws, liked_num)
     if len(ws) < 100:
         ws[0] += (100 - ws.sum())
     
@@ -49,7 +50,12 @@ def insert_recommendation_rank(user_id, index=0,image_vector_rank_=None):
         image_vector_rank = image_vector_rank_
         
     
-    character_rank = get_character_rank(user_id=user_id)['rank_list']
+    character_rank = get_character_rank(user_id=user_id)
+    if character_rank is None:
+        ws[3] = 0
+        ws[0] += (100 - ws.sum())
+    else:
+        character_rank = character_rank['rank_list']
     latest_rank = get_latest_dog_rank()['rank_list']
     # liked_rank = get_liked_dog_rank()['rank_list']
     # liked_rank_num = len(liked_rank)
@@ -59,13 +65,16 @@ def insert_recommendation_rank(user_id, index=0,image_vector_rank_=None):
     # TODO: ws 합치는거 수정하기. liked 2000개 쌓이면 풀어주기
     result_list.extend(latest_rank[index*(ws[0]+ws[1]):(index+1)*(ws[0]+ws[1])])
     # result_list.extend(liked_rank[index*ws[1]:(index+1)*ws[1]])
-    result_list.extend(image_vector_rank[index*ws[2]:(index+1)*ws[2]])
-    result_list.extend(character_rank[index*ws[3]:(index+1)*ws[3]])
+    if ws[2] > 0:
+        result_list.extend(image_vector_rank[index*ws[2]:(index+1)*ws[2]])
+    if ws[3] > 0 and character_rank is not None:
+        result_list.extend(character_rank[index*ws[3]:(index+1)*ws[3]])
     
     if len(result_list) != 100:
         raise ValueError(f'not length 100 : {len(result_list)=}')
-    print(result_list)
-    # upsert_user_recommendation_rank(user_id=user_id, rank_list=result_list)
+
+    # print(result_list)
+    upsert_user_recommendation_rank(user_id=user_id, rank_list=result_list)
 
 def recommedate(user_id_list):
     for user_id in user_id_list:
