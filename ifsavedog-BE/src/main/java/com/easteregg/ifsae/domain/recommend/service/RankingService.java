0e61fcaf1bd088b84d8cd67ff8b662275041ebb0
김재현 <wogus565@gmail.com>
2024-10-10 04:30:30 +0900

    [BE] feat : 유저별로 pageNation해서 추천 영상을 5개씩 List해서 API보내는 기능

--- ifsavedog-BE/src/main/java/com/easteregg/ifsae/domain/recommend/service/RankingService.java
+++ ifsavedog-BE/src/main/java/com/easteregg/ifsae/domain/recommend/service/RankingService.java
@@ -1,9 +1,11 @@
 package com.easteregg.ifsae.domain.recommend.service;
 
-import com.easteregg.ifsae.domain.dog.entity.Dog;
+import com.easteregg.ifsae.domain.post.dto.PostDto;
 import java.util.List;
 
 public interface RankingService {
 
-    List<Dog> findRankingsByUserId(Long userId);
+	List<Long> findDogIdListByUserId(Long userId, int pageNum);
+
+	List<PostDto.Response> findPostDogListByDogIds(List<Long> dogIds);
 }
