f48b8ba9ba908cf5dd285b35d4add16ca0a3ba33
kgc91747 <kgc91747@naver.com>
2024-10-10 07:25:00 +0900

    [BE]feat: lastPageRedisRepo 추가
    - lastPageRedisRepo 추가
    - 유저의 마지막 시청 페이지를 조회하는 API 추가

---
+++ ifsavedog-BE/src/main/java/com/easteregg/ifsae/domain/recommend/entity/LastPage.java
@@ -0,0 +1,17 @@
+package com.easteregg.ifsae.domain.recommend.entity;
+
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+@Getter
+@Builder
+@RedisHash(value = "LastPage")
+public class LastPage {
+
+    @Id
+    private Long userId;
+
+    private int lastPage;
+}
