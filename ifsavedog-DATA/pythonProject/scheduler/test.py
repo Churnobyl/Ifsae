2f6d69fc52bda95a23aaa01825794d92e23eb398
김재현 <wogus565@gmail.com>
2024-10-09 20:29:39 +0900

    [DATA] Test : testfile수정

--- ifsavedog-DATA/pythonProject/scheduler/test.py
+++ ifsavedog-DATA/pythonProject/scheduler/test.py
@@ -3,20 +3,8 @@ from job.makes_movie import *
 from job.recommend_by_character import *
 from job.input_all_datas import *
 
-<<<<<<< Updated upstream
-# image_name = 'KakaoTalk_20241007_134037647.jpg'
-# make_movie(image_name)
-user_list = {19,1,29}
-calculate_dog_character_score(user_list)
-# print(get_all_users())
-# download_dog_image_boot('2024070')
-# print(len(get_all_image_vector()))
-=======
-input_dog_species()
-
-# dogs = get_all_dogs()
-# for dog in dogs:
-#   # dog = get_dog_by_id(36)
-#   input_post_datas_and_match_post_dog(dog)
-#   if dog.id == 120: break
->>>>>>> Stashed changes
+dogs = get_all_dogs()
+for dog in dogs:
+  dog = get_dog_by_id(36)
+  input_post_datas_and_match_post_dog(dog)
+  if dog.id == 120: break
