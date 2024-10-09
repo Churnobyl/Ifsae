658d272a9e71c6d87a047de31ed8cbafe213dddc
SungyoonRyu <rara0620@naver.com>
2024-10-10 00:43:51 +0900

    [DATA]Test : modify test file

--- ifsavedog-DATA/pythonProject/scheduler/test.py
+++ ifsavedog-DATA/pythonProject/scheduler/test.py
@@ -1,6 +1,7 @@
 from service.get_datas import *
 from job.makes_movie import *
 from job.recommend_by_character import *
+from job.input_all_datas import *
 
 # image_name = 'KakaoTalk_20241007_134037647.jpg'
 # make_movie(image_name)
