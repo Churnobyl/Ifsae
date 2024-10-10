6f10d783a8e3e9223ab8054e1c07e296a17450a6
서경덕 <skdb0105@naver.com>
2024-10-10 10:02:53 +0900

    [BE]fix : es 검색 시 멀티 필드로 변경

--- ifsavedog-BE/src/main/java/com/easteregg/ifsae/global/elasticsearch/service/SearchServiceImpl.java
+++ ifsavedog-BE/src/main/java/com/easteregg/ifsae/global/elasticsearch/service/SearchServiceImpl.java
@@ -21,28 +21,19 @@ public class SearchServiceImpl implements SearchService {
     private final ElasticsearchClient elasticsearchClient;
 
     @Override
-    public List<Long> searchDogs(String query, String searchField) throws IOException {
-        /*
-        개 검색 조건
-            - name
-            - species
-            - shelterName
-         */
+    public List<Long> searchDogs(String query) throws IOException {
+    /*
+    개 검색 조건
+        - name
+        - species
+        - shelterName
+     */
         Query boolQuery = BoolQuery.of(b -> {
-            if ("name".equalsIgnoreCase(searchField)) {
-                b.should(s -> s.matchPhrase(m -> m.field("name").query(query)));
-            } else if ("species".equalsIgnoreCase(searchField)) {
-                b.should(s -> s.matchPhrase(m -> m.field("species").query(query)));
-            } else if ("shelterName".equalsIgnoreCase(searchField)) {
-                b.should(s -> s.nested(n -> n
-                        .path("shelter")
-                        .query(q -> q.matchPhrase(m -> m.field("shelter.name").query(query)))));
-            }
-
+            b.should(s -> s.multiMatch(m -> m
+                    .query(query)
+                    .fields("name", "species", "shelter.name")));
             b.minimumShouldMatch("1");
-
             return b;
-
         })._toQuery();
 
         SearchResponse<ESDog> searchResponse = elasticsearchClient.search(
@@ -64,36 +55,20 @@ public class SearchServiceImpl implements SearchService {
     }
 
     @Override
-    public List<Long> searchPosts(String query, String searchField) throws IOException {
-        /*
-        게시글 검색 조건
-            - title
-            - content
-            - dogName
-            - shelterName
-            - userNickname
-         */
+    public List<Long> searchPosts(String query) throws IOException {
+    /*
+    게시글 검색 조건
+        - title
+        - content
+        - dogName
+        - shelterName
+        - userNickname
+     */
         Query boolQuery = BoolQuery.of(b -> {
-            if ("title".equalsIgnoreCase(searchField)) {
-                b.should(s -> s.matchPhrase(m -> m.field("title").query(query)));
-            }
-            else if ("content".equalsIgnoreCase(searchField)) {
-                b.should(s -> s.matchPhrase(m -> m.field("content").query(query)));
-            }
-            else if ("dogName".equalsIgnoreCase(searchField)) {
-                b.should(s -> s.matchPhrase(m -> m.field("dogName").query(query)));
-            }
-            else if ("shelterName".equalsIgnoreCase(searchField)) {
-                b.should(s -> s.nested(n -> n
-                        .path("shelter")
-                        .query(q -> q.matchPhrase(m -> m.field("shelter.name").query(query)))));
-            }
-            else if ("userNickname".equalsIgnoreCase(searchField)) {
-                b.should(s -> s.matchPhrase(m -> m.field("userNickname").query(query)));
-            }
-
+            b.should(s -> s.multiMatch(m -> m
+                    .query(query)
+                    .fields("title", "content", "dogName", "shelter.name", "userNickname")));
             b.minimumShouldMatch("1");
-
             return b;
         })._toQuery();
 
