aa219807682676b342c8953a4f127f2f4f2e406c
성철민 <tjdcjfals3@naver.com>
2024-10-10 16:30:26 +0900

    [FE][Fix] Dockerfile

--- ifsavedog-FE/env.sh
+++ ifsavedog-FE/env.sh
@@ -1,20 +1,17 @@
 #!/bin/sh
 
-# Debug: Print environment variables to check if they are set
+# 환경 변수를 echo로 출력하여 확인 (디버깅 용도)
 echo "VITE_BASE_URL: $VITE_BASE_URL"
 echo "VITE_AXIOS_TIMEOUT: $VITE_AXIOS_TIMEOUT"
 echo "VITE_COOKIE_NAME_FOR_LANDING_PAGE: $VITE_COOKIE_NAME_FOR_LANDING_PAGE"
 echo "VITE_COOKIE_MAX_AGE: $VITE_COOKIE_MAX_AGE"
 echo "VITE_S3_VIDEO_URL: $VITE_S3_VIDEO_URL"
 
-# Create env-config.js file
-echo "window._env_ = {" > ./env-config.js
-echo "  VITE_BASE_URL: '$VITE_BASE_URL'," >> ./env-config.js
-echo "  VITE_AXIOS_TIMEOUT: $VITE_AXIOS_TIMEOUT," >> ./env-config.js
-echo "  VITE_COOKIE_NAME_FOR_LANDING_PAGE: '$VITE_COOKIE_NAME_FOR_LANDING_PAGE'," >> ./env-config.js
-echo "  VITE_COOKIE_MAX_AGE: $VITE_COOKIE_MAX_AGE," >> ./env-config.js
-echo "  VITE_S3_VIDEO_URL: '$VITE_S3_VIDEO_URL'" >> ./env-config.js
-echo "};" >> ./env-config.js
-
-# Debug: Show contents of env-config.js to verify it's created
-cat ./env-config.js
+# env-config.js 파일을 생성
+echo "window._env_ = {" > ./dist/env-config.js
+echo "  VITE_BASE_URL: '$VITE_BASE_URL'," >> ./dist/env-config.js
+echo "  VITE_AXIOS_TIMEOUT: $VITE_AXIOS_TIMEOUT," >> ./dist/env-config.js
+echo "  VITE_COOKIE_NAME_FOR_LANDING_PAGE: '$VITE_COOKIE_NAME_FOR_LANDING_PAGE'," >> ./dist/env-config.js
+echo "  VITE_COOKIE_MAX_AGE: $VITE_COOKIE_MAX_AGE," >> ./dist/env-config.js
+echo "  VITE_S3_VIDEO_URL: '$VITE_S3_VIDEO_URL'" >> ./dist/env-config.js
+echo "};" >> ./dist/env-config.js
