b8f08fc0f9a5a6525b650fe975e551986ff11b30
성철민 <tjdcjfals3@naver.com>
2024-10-10 16:03:04 +0900

    [FE][Fix] env.sh

---
+++ ifsavedog-FE/env-config.js
@@ -0,0 +1,8 @@
+window._env_ = {
+  VITE_BASE_URL: 'http://localhost:8081',
+  VITE_AXIOS_TIMEOUT: 6,
+  VITE_COOKIE_NAME_FOR_LANDING_PAGE: 'hasViewed',
+  VITE_COOKIE_MAX_AGE: 604800,
+  VITE_S3_VIDEO_URL:
+    'https://ifsavedog-bucket-1.s3.ap-southeast-2.amazonaws.com/',
+};
--- ifsavedog-FE/env.sh
+++ ifsavedog-FE/env.sh
@@ -9,11 +9,11 @@ echo "VITE_S3_VIDEO_URL: $VITE_S3_VIDEO_URL"
 
 # Create env-config.js file
 echo "window._env_ = {" > ./dist/env-config.js
-echo "  VITE_BASE_URL: '${VITE_BASE_URL}'," >> ./dist/env-config.js
-echo "  VITE_AXIOS_TIMEOUT: ${VITE_AXIOS_TIMEOUT}," >> ./dist/env-config.js
-echo "  VITE_COOKIE_NAME_FOR_LANDING_PAGE: '${VITE_COOKIE_NAME_FOR_LANDING_PAGE}'," >> ./dist/env-config.js
-echo "  VITE_COOKIE_MAX_AGE: '${VITE_COOKIE_MAX_AGE}'," >> ./dist/env-config.js
-echo "  VITE_S3_VIDEO_URL: ${VITE_S3_VIDEO_URL}" >> ./dist/env-config.js
+echo "  VITE_BASE_URL: '$VITE_BASE_URL'," >> ./dist/env-config.js
+echo "  VITE_AXIOS_TIMEOUT: $VITE_AXIOS_TIMEOUT," >> ./dist/env-config.js
+echo "  VITE_COOKIE_NAME_FOR_LANDING_PAGE: '$VITE_COOKIE_NAME_FOR_LANDING_PAGE'," >> ./dist/env-config.js
+echo "  VITE_COOKIE_MAX_AGE: $VITE_COOKIE_MAX_AGE," >> ./dist/env-config.js
+echo "  VITE_S3_VIDEO_URL: '$VITE_S3_VIDEO_URL'" >> ./dist/env-config.js
 echo "};" >> ./dist/env-config.js
 
 # Debug: Show contents of env-config.js to verify it's created
