9962b27f70665ef927b66e4a45770cbece9ee195
김재현 <wogus565@gmail.com>
2024-10-10 16:40:10 +0900

    [DATA] feat : spring과 api 소통하기 위한 코드

--- ifsavedog-DATA/pythonProject/app/api/v1/endpoints.py
+++ ifsavedog-DATA/pythonProject/app/api/v1/endpoints.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 from sqlalchemy.orm import Session
 from app.db.maria import get_db
 from app.services.user_service import get_user_by_id
@@ -10,4 +10,15 @@ router = APIRouter()
 # dog 조회 API 
 @router.get("/dogs/desertionNo/{desertion_no}", response_model=DogResponse)
 async def retrieve_dog_by_desertion_no(desertion_no: str):    
-    return await get_dog_by_desertion_no(desertion_no)
+    return await get_dog_by_desertion_no(desertion_no) 
+
+# Spring에서 쏘는 POST 요청을 받을 API
+@router.post("/ranking")
+async def receive_ranking_data(request: Request, db: Session = Depends(get_db)):
+    data = await request.json()
+    if isinstance(data, dict):
+        user_id = data.get("userId")
+    else:
+        user_id = data
+    # 받은 userId를 출력하거나 원하는 로직 처리
+    return {"status": "success"}
--- ifsavedog-DATA/pythonProject/app/main.py
+++ ifsavedog-DATA/pythonProject/app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from motor.motor_asyncio import AsyncIOMotorClient
 from app.api.v1.endpoints import router as api_router
 from beanie import init_beanie
 from contextlib import asynccontextmanager
 from app.db.mongo import connect_to_mongo, close_mongo_connection
 from app.models.dog import Dog
+from app.core.config import CORS_CONFIG
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -16,5 +18,13 @@ async def lifespan(app: FastAPI):
         await close_mongo_connection()
 
 app = FastAPI(lifespan=lifespan)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=CORS_CONFIG["allow_origins"],
+    allow_credentials=CORS_CONFIG["allow_credentials"],
+    allow_methods=CORS_CONFIG["allow_methods"],
+    allow_headers=CORS_CONFIG["allow_headers"],
+)
 
 app.include_router(api_router) 
