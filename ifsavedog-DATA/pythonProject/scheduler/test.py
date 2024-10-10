d1019b8d28a0957b7860b25ab81406beab11d0e0
김재현 <wogus565@gmail.com>
2024-10-10 16:43:08 +0900

    [DATA] Test : test코드 수정

--- ifsavedog-DATA/pythonProject/scheduler/test.py
+++ ifsavedog-DATA/pythonProject/scheduler/test.py
@@ -4,16 +4,16 @@ from job.recommend_by_character import *
 from job.input_all_datas import *
 from job.input_user import *
 
-match_shelter_user()  
+# match_shelter_user()  
 
 # make_user()
 
-# start_processing = False
-# dogs = get_all_dogs()
-# for dog in dogs:
-#   if dog.id == 545:
-#         start_processing = True
+start_processing = False
+dogs = get_all_dogs()
+for dog in dogs:
+  if dog.id == 7674:
+        start_processing = True
 
-#   if start_processing:
-#       input_post_datas_and_match_post_dog(dog)
-#       print(dog.id)
+  if start_processing:
+      input_post_datas_and_match_post_dog(dog)
+      print(dog.id)
