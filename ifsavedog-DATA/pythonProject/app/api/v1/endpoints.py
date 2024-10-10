ac1a68ae5573a87c973bca0db8cd6983c327a878
SungyoonRyu <rara0620@naver.com>
2024-10-10 16:32:37 +0900

    [DATA]Feat : add user rank api

--- ifsavedog-DATA/pythonProject/app/api/v1/endpoints.py
+++ ifsavedog-DATA/pythonProject/app/api/v1/endpoints.py
@@ -1,9 +1,20 @@
-from fastapi import APIRouter, Depends
+import os
+import sys
+from fastapi import APIRouter, Depends, Response
 from sqlalchemy.orm import Session
 from app.db.maria import get_db
 from app.services.user_service import get_user_by_id
 from app.services.dog_service import get_dog_by_desertion_no
 from app.models.dog import DogResponse
+
+from dotenv import load_dotenv
+load_dotenv()
+
+package_root = os.getenv("PYTHONPATH")
+if package_root:
+    sys.path.insert(0, package_root)
+
+from scheduler.service.recommendation import insert_recommendation_rank
 
 router = APIRouter()
 
@@ -12,7 +23,11 @@ router = APIRouter()
 async def retrieve_dog_by_desertion_no(desertion_no: str):    
     return await get_dog_by_desertion_no(desertion_no)
 
-@router.post("/api/recommendation")
-async def recommendation(user_id: int, db: Session = Depends(get_db)):
-    user = get_user_by_id(db, user_id)
-    return await get_dog_by_desertion_no(user.desertion_no)
+@router.get("/rank/{user_id}")
+async def reset_user_rank(user_id: int, page: int = 0):
+    try:
+        insert_recommendation_rank(user_id=user_id, index=page)
+        return Response(content= 'b', status_code=200)
+    except Exception as e:
+        print(e)
+        return
